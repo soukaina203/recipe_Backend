96d91d11a4be7188c6814ec0ffc3059fc4a0a1fd
soukaina203 <soukainamourabit9@gmail.com>
2025-07-30 23:49:08 +0100

    Feat : retriving recipe comments and likes

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -10,36 +10,53 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Controllers
 {
-    [ApiController]
-    [Route("api/[controller]/[action]")]
-    public class RecipeController : SuperController<Recipe>
-    {
-          public RecipeController(MyContext context) : base(context)
-        {
-        }
+	[ApiController]
+	[Route("api/[controller]/[action]")]
+	public class RecipeController : SuperController<Recipe>
+	{
+		  public RecipeController(MyContext context) : base(context)
+		{
+		}
 
-                [HttpGet("")]
-        public override async Task<IActionResult> GetAll()
-        {
-            // Custom implementation for RecipeController
-            var recipes = await _context.Recipes
-                                        .Include(r => r.User) // Assuming a relationship with Ingredients
-                                        .Include(r => r.Category) // Assuming a relationship with Ingredients
-                                        .ToListAsync();
-            return Ok(recipes );
-        }
+				[HttpGet("")]
+		public override async Task<IActionResult> GetAll()
+		{
+			// Custom implementation for RecipeController
+			var recipes = await _context.Recipes
+										.Include(r => r.User) // Assuming a relationship with Ingredients
+										.Include(r => r.Category) // Assuming a relationship with Ingredients
+										.ToListAsync();
+			return Ok(recipes );
+		}
 
-    [HttpGet("{id}")]
-    public override async Task<IActionResult> GetById(int id)
-        {
-            // Custom implementation for RecipeController
-   var model = await _context.Recipes
-                              .Include(r => r.Category)
-                              .FirstOrDefaultAsync(r => r.Id == id);
-            return Ok(model);
-        }
+	[HttpGet("{id}")]
+	public override async Task<IActionResult> GetById(int id)
+		{
+			// Custom implementation for RecipeController
+		var recipe = await _context.Recipes
+			.Include(r => r.Category)
+			.FirstOrDefaultAsync(r => r.Id == id);
 
+		var recipeLikes = await _context.Likes
+			.Include(r => r.User)
+			.Where(l => l.IdRecipe == id)
+			.ToListAsync();
 
+		var recipeComments = await _context.Comments
+			.Include(r => r.User)
+		
+			.Where(c => c.IdRecipe == id)
+			.ToListAsync();
 
-    }
+		return Ok(new
+		{
+			recipe = recipe,
+			likes = recipeLikes,
+			comments = recipeComments
+		});
+
+	}
+	}
+	
+	
 }
